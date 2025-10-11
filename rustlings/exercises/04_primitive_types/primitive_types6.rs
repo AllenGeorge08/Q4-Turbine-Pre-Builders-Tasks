0dda9d14a3779e8ea24a29492a3be174c51552cd
Allen <allengeorge2400@gmail.com>
2025-10-11 19:40:23 +0000

    Completed fourth exercisse data types

--- rustlings/exercises/04_primitive_types/primitive_types6.rs
+++ rustlings/exercises/04_primitive_types/primitive_types6.rs
@@ -1,5 +1,10 @@
 fn main() {
     // You can optionally experiment here.
+
+    let numbers  =(22,23,24);
+    let mut first = 0;
+    (first,_,_) = numbers;
+    println!("{:?}",first);
 }
 
 #[cfg(test)]
@@ -10,6 +15,8 @@ mod tests {
 
         // TODO: Use a tuple index to access the second element of `numbers`
         // and assign it to a variable called `second`.
+        let mut second = 0;
+        (_,second,_) = numbers ;
         // let second = ???;
 
         assert_eq!(second, 2, "This is not the 2nd number in the tuple!");
