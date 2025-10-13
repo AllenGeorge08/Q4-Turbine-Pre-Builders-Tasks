4681fde43f99a95219121860211404a5497e68be
Allen <allengeorge2400@gmail.com>
2025-10-13 02:24:55 +0000

    Completed 7th and 8th modules of the exercise

--- rustlings/exercises/09_strings/strings3.rs
+++ rustlings/exercises/09_strings/strings3.rs
@@ -1,22 +1,40 @@
 fn trim_me(input: &str) -> &str {
     // TODO: Remove whitespace from both ends of a string.
+    let output = input.trim();
+    output 
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this.
+
+    //e Owned string is a heap allocated,growable string managed by your program .It ownss this data, meaning it:
+    //Controls the memory, can grow or shrink,gets automatically cleaned up when it goes 
+    input.to_string() + "world"  //e It requires a string on the left and a &str on the right
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons".
+    let replaced_string = input.replace("cars", "balloons");
+    replaced_string
 }
 
 fn main() {
     // You can optionally experiment here.
+
+    let hello = String::from("      Hello,World!!       ");
+    println!("{}",hello.trim());
+
+    let string = "Abel Cherian Babu";
+    let replaced_string = string.replace("Abel Cherian Babu", "Allen George Babu");
+    println!("{}",replaced_string);
+
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    
 
     #[test]
     fn trim_a_string() {
